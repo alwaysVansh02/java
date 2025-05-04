06fa60b1e81466b37a01334248cc2b8e9694be46
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-04 11:20:37 +0530

    Create  areaofcicle.java

---
+++ areaofcicle.java
@@ -0,0 +1,10 @@
+package BasicOfJava;
+
+public class areaofcicle {
+    public static void main(String[] args) {
+        double d=20.0;
+        double r = d/2;
+        double a = 3.14 * r * r;
+        System.out.println("the value of a is " + a);
+    }
+}
