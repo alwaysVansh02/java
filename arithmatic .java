4baec17d2bd829a2b48aa0e928e0af0ec7df13b7
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-05 19:03:45 +0530

    Create arithmatic .java

---
+++ arithmatic .java
@@ -0,0 +1,14 @@
+package BasicOfJava;
+
+public class arithmatic {
+    public static void main(String[] args) {
+        int x = 2, y = 5, z = 8;
+        System.out.println("the value of x is " + x);
+        System.out.println(" the value of y is " + y);
+        z = 9;
+        z = x + y;
+        System.out.println("the valuen of z is " + z);
+        System.out.println(x + y + z );
+      System.out.println(  x + y +" "+ z);
+    }
+}
