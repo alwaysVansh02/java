0d47bcdd3b4559745071ff3ad1dc8b219e6943a6
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-06 12:52:54 +0530

    Create doubledatatype.java

---
+++ doubledatatype.java
@@ -0,0 +1,9 @@
+package BasicOfJava;
+
+public class doubledatatype {
+    public static void main(String[] args) {
+        double x = 3;
+        double y = 2;
+        System.out.println(3/2);
+    }
+}
