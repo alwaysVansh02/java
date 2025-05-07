4bbfacee14fb6f43350ad57a8ad0a15081db7e54
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-07 19:04:46 +0530

    Create percentageofstudents.java

---
+++ basic_of_java/percentageofstudents.java
@@ -0,0 +1,13 @@
+package BasicOfJava;
+
+public class percentageofstudents {
+    public static void main(String[] args) {
+        double x1 = 95;
+        double x2 = 85;
+        double x3 = 95;
+        double x4 = 86;
+        double x5 = 92;
+        double y = (x1 + x2 + x3+ x4 + x5)/5;
+        System.out.println(y);
+    }
+}
