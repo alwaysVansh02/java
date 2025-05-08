00ec1db948ca04e5d558a20772fdffc85e327185
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-08 14:25:12 +0530

    Create simpleinterewst.java

---
+++ basic_of_java/simpleinterewst.java
@@ -0,0 +1,12 @@
+package BasicOfJava;
+
+public class simpleinterewst {
+    public static void main(String[] args) {
+        double p = 100;
+        double r = 20;
+        double t = 2;
+        double si = (p * r * t)/100;
+        System.out.println(si);
+
+    }
+}
