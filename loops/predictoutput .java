0956503111f3422daf5336772e1273ffa48cc774
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-28 10:22:11 +0530

    Create  predictoutput .java

---
+++ loops/predictoutput .java
@@ -0,0 +1,21 @@
+package Loops;
+import java.util.*;
+public class predictoutput {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter a number :");
+      int n = sc.nextInt();
+      int count = 0;
+      while(n!=0){
+          n/=10;
+          count++;
+      }
+        System.out.println(count);
+
+       }
+
+       }
+
+
+
+
