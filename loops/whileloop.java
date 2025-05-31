3532ffbce0cd05bbc4e042d93883a24b6d08865b
Vansh  S <vanshsinghrajput746@gmail.com>
2025-05-31 18:07:50 +0530

    Create whileloop.java

---
+++ loops/whileloop.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class whileloop {
+    public static void main(String[] args) {
+//        for(int i=1;i<=10;i++){
+//            System.out.println(i+" ");
+        int i=1;
+        do{
+            System.out.println(i+" ");
+            i++;
+        }
+        while(i<=10);
+    }
+}
