0fc863b3cdf6f07f38ed928dd3835e03ec40fd37
Vansh  S <vanshsinghrajput746@gmail.com>
2025-06-04 21:25:06 +0530

    Create passingargumentsv.java

---
+++ methods/passingargumentsv.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class passingarguments {
+    public static void intro(String name, int age, String std, char grade){
+        System.out.println( " hello " + name +  " your age is "+ age + " your std is "+ std + " grade is "+ grade);
+    }
+public static void main (String[] args){
+intro(" vansh ",20 ," fifth ", 'A'); /// upar wale ke according hi receive hona chahiye jaise pehle name,std , age,grade
+
+}
+
+}
+
