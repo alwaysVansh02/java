f6cac3f3936e65a0ff558012a562631c913d1bf7
Vansh  S <vanshsinghrajput746@gmail.com>
2025-06-13 09:24:50 +0530

    Create pyramid.java

---
+++ patter printing/pyramid.java
@@ -0,0 +1,23 @@
+package pattern_printing;
+
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {   //for lines
+            for (int j = 1; j <= n - i; j++) {   // for spaces
+                System.out.print(" "+" ");
+            }
+
+                for (int j = 1; j <= 2 * i - 1; j++) {  // for star
+                    System.out.print(" * " + " ");
+
+                }
+                System.out.println();
+            }
+        }
+    }
+
