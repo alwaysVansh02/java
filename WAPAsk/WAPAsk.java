a9f711676822815a6423be74b6982867bc08c235
Vansh  S <vanshsinghrajput746@gmail.com>
2025-07-02 17:58:51 +0530

    Create  WAPAsk.java

---
+++ WAPAsk/WAPAsk.java
@@ -0,0 +1,11 @@
+package WAPQuestions;
+public class WAPAsk {
+ public static void main(String[] args) {
+        int a= 10, b = 20;
+        System.out.println(" before swaoing the numbers are: " +a+" " +b);
+        int temp = a;
+        a=b;
+        b= temp;
+        System.out.print(" after swaping numbers are : " +a +" " +b);
+ }
+}
