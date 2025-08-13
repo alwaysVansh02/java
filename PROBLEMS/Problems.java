c61ee8a63062281a0ffdf9b534b9c2bfcdf298db
Vansh  S <vanshsinghrajput746@gmail.com>
2025-08-13 20:30:06 +0530

    Create Problems.java

---
+++ PROBLEMS/Problems.java
@@ -0,0 +1,17 @@
+package LEETCODEProblems;
+
+//import java.util.Scanner;
+//
+public class Problems {
+    public static void main(String[] args) {
+int i = 42;
+double j = 3.1415;
+
+        String k = " Welcome to HackerRank";
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+
+
+    }
+}
