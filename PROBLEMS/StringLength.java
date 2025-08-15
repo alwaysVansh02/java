50bf7ca1df3b31cadb29b21468e25d64a863eb8d
Vansh  S <vanshsinghrajput746@gmail.com>
2025-08-15 22:29:36 +0530

    Create  StringLength.java

---
+++ PROBLEMS/StringLength.java
@@ -0,0 +1,14 @@
+package PROBLEMS;
+
+public class StringLength {
+    public static void main(String[] args) {
+        // Example string
+        String str = "Hello World";
+
+        // Finding length using length() method
+        int length = str.length();
+
+        // Printing the length
+        System.out.println("Length of the string: " + length);
+    }
+}
