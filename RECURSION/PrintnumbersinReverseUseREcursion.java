7652fa3579807146edf68de88fce58dbb56c4e23
Vansh  S <vanshsinghrajput746@gmail.com>
2025-08-17 17:14:52 +0530

    Create  PrintnumbersinReverseUseREcursion.java

---
+++ RECURSION/PrintnumbersinReverseUseREcursion.java
@@ -0,0 +1,23 @@
+package RECURSION;
+
+import java.util.Scanner;
+
+public class PrintnumbersinReverseUseREcursion {
+    static void printdecrease(int n ){
+        // base case
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        // self work
+        System.out.println(n);
+        // recursive work
+        printdecrease(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        printdecrease(n);
+    }
+}
