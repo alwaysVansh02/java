d4034a828fa844d2e07241c5e95354147514adf8
Vansh  S <vanshsinghrajput746@gmail.com>
2025-09-23 23:05:32 +0530

    Create GFG_ObjectsUsing_Function.java

---
+++ GFG_Java_Question.java/GFG_ObjectsUsing_Function.java
@@ -0,0 +1,22 @@
+package GFGJavaQuestion;
+
+public class Geeks {
+
+    // by using funtion
+    static String name;
+    static float price;
+
+    static void set(String n, float p){
+        name  = n;
+        price =p;
+    }
+    static void get(){
+        System.out.println("Software name is:"+ name);
+        System.out.println("Software price is:"+ price);
+    }
+
+    public static void main(String[] args) {
+        Geeks.set("Visual studio", 0.2f);
+        Geeks.get();
+    }
+}
