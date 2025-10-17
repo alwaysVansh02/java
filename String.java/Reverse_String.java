22ca7db7a929ac1753c99f0e065a2bb0244c1389
Vansh  S <vanshsinghrajput746@gmail.com>
2025-10-17 14:48:25 +0530

    Create Reverse_String.java

---
+++ String.java/Reverse_String.java
@@ -0,0 +1,12 @@
+package String_Problems;
+
+public class Reverse_String {
+    public static void main(String[] args) {
+        String str = "INTERVIEW";
+
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+
+        }
+    }
+
