501325b7ca114b548d309688e9fdda6f3d5e78aa
Vansh  S <vanshsinghrajput746@gmail.com>
2026-04-15 23:21:21 +0530

    Update FibonnaciSeriesWithRecursion,.java

--- RECURSION/FibonnaciSeriesWithRecursion,.java
+++ RECURSION/FibonnaciSeriesWithRecursion,.java
@@ -6,7 +6,7 @@ public class FibonnaciSeriesWithRecursion {
         if(n==0 || n== 1){
             return n;
         }. 
-        // rceurrsive work
+        // rceurrsive work 
         int prev1 = fib(n-1);
         int prev2 = fib(n-2);
         // self work
