0095b7e1ca420e739990a7e9fa83b0ddd0048e4b
Vansh  S <vanshsinghrajput746@gmail.com>
2026-07-03 22:55:36 +0530

    Fix formatting in Problems.java

--- PROBLEMS/Problems.java
+++ PROBLEMS/Problems.java
@@ -6,7 +6,7 @@ public class Problems {
     public static void main(String[] args) {
 int i = 42;
 double j = 3.1415;
-
+ 
         String k = " Welcome to HackerRank";
         System.out.println(i);
         System.out.println(j);
