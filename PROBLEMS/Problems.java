b6a42a7111f6e84a2b9bb9d19469eb0b92dc8488
Vansh  S <vanshsinghrajput746@gmail.com>
2026-07-20 23:05:46 +0530

    Fix formatting of string initialization

--- PROBLEMS/Problems.java
+++ PROBLEMS/Problems.java
@@ -7,7 +7,7 @@ public class Problems {
 int i = 42;
 double j = 3.1415;
  
-        String k = " Welcome to HackerRank";
+        String k = " Welcome to HackerRank"; 
         System.out.println(i);
         System.out.println(j);
         System.out.println(k);
